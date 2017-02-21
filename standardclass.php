980cd036c284bed39ce0754deaa3ab5251a9e53a
dofeldsc <dofeldsc@uos.de>
2017-02-21 16:22:27 +0100

    Added parse function

--- standardclass.php
+++ standardclass.php
@@ -8,5 +8,17 @@ class standardclass
 	public function test_func() {
 		echo $this->var;
 	}
+
+
+	public function parse_proforma($result_string) {
+	    $xml = simplexml_load_string($result_string);
+
+	    $percent = "";
+	    $comment = "";
+	    $output  = "";
+
+
+
+    }
 }
 ?>
